577fadfc49e605116c9e2f347b3668cf6a27c61e
Loris Michel <loris.michel@gmail.com>
2020-01-29 15:14:01 +0100

    changed experiment

--- experiments/copula_examples/copula_example.R
+++ experiments/copula_examples/copula_example.R
@@ -14,20 +14,30 @@ X <- cbind(x.active, x.noise)
 Y <- t(apply(X, 1, function(xx) MASS::mvrnorm(n = 1, mu = c(0,0), Sigma = Sigma.fun(xx[1]))))
 
 require(mrf)
-mRF_fourier <- mrf(X = X, Y = Y, num.trees = 500, splitting.rule = "fourier", num_features = 50)
-mRF_gini <- mrf(X = X, Y = Y, num.trees = 500, splitting.rule = "gini", num_features = 50)
+mRF_fourier_node_scaling <- mrf(X = X, Y = Y, num.trees = 500, splitting.rule = "fourier", num_features = 50,  bandwidth = 1, node_scaling = TRUE, min.node.size = 20)
+mRF_fourier <- mrf(X = X, Y = Y, num.trees = 500, splitting.rule = "fourier", num_features = 50,  bandwidth = 1, node_scaling = FALSE, min.node.size = 20)
+mRF_gini <- mrf(X = X, Y = Y, num.trees = 500, splitting.rule = "gini", num_features = 50, bandwidth = 1, min.node.size = 20)
+p_fourier_node_scaling <- predict(mRF_fourier_node_scaling, newdata = cbind(seq(-1,1,length.out = 15), matrix(0, ncol=19,nrow=15)))
 p_fourier <- predict(mRF_fourier, newdata = cbind(seq(-1,1,length.out = 15), matrix(0, ncol=19,nrow=15)))
 p_gini <- predict(mRF_gini, newdata = cbind(seq(-1,1,length.out = 15), matrix(0, ncol=19,nrow=15)))
 
 
+# produce plots 
+par(mfrow=c(4,4))
+#par(mar=rep(2,4))
+plot(col="black",p_fourier_node_scaling$y,pch=19,main="original data",cex=0.2)
+for (i in 1:nrow(p_fourier_node_scaling$weights)) {
+  plot(col="darkblue", p_fourier_node_scaling$y, cex=p_fourier_node_scaling$weights[i,]*200,pch=19, asp=1, main=paste0("X1=",round(seq(-1,1,length.out = 16)[i],3)))
+  #plotBivariate(correl = FALSE, col="darkblue", x = p_fourier$y[,1], y = p_fourier$y[,2], cex.points = p_fourier$weights[i,]*200,pch=19, asp=1, main=paste0("X1=",round(seq(-1,1,length.out = 16)[i],3)))
+}
 
 # produce plots 
 par(mfrow=c(4,4))
 #par(mar=rep(2,4))
 plot(col="black",p_fourier$y,pch=19,main="original data",cex=0.2)
 for (i in 1:nrow(p_fourier$weights)) {
-  #plot(col="darkblue", p_fourier$y, cex=p_fourier$weights[i,]*200,pch=19, asp=1, main=paste0("X1=",round(seq(-1,1,length.out = 16)[i],3)))
-  plotBivariate(correl = FALSE, col="darkblue", x = p_fourier$y[,1], y = p_fourier$y[,2], cex.points = p_fourier$weights[i,]*200,pch=19, asp=1, main=paste0("X1=",round(seq(-1,1,length.out = 16)[i],3)))
+  plot(col="darkblue", p_fourier$y, cex=p_fourier$weights[i,]*200,pch=19, asp=1, main=paste0("X1=",round(seq(-1,1,length.out = 16)[i],3)))
+  #plotBivariate(correl = FALSE, col="darkblue", x = p_fourier$y[,1], y = p_fourier$y[,2], cex.points = p_fourier$weights[i,]*200,pch=19, asp=1, main=paste0("X1=",round(seq(-1,1,length.out = 16)[i],3)))
   }
 
 par(mfrow=c(4,4))
